811cd2682f6cdc05af38aa866c02c850cf93dbee
Proteeti Sinha <Proteeti@MacBook-Pro-4.local>
2019-07-13 17:11:38 +0530

    Multiple filters

--- src/App.js
+++ src/App.js
@@ -1,88 +1,151 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
+/* import { DropdownMenu, MenuItem } from 'react-bootstrap-dropdown-menu'; */
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-/*import Chip from '@material-ui/core/Chip';*/
-import { DropdownMenu, MenuItem } from 'react-bootstrap-dropdown-menu';
-import Grow from '@material-ui/core/Grow';
-import TextField from '@material-ui/core/TextField';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
-import ClearIcon from '@material-ui/icons/Clear';
-import { withStyles } from '@material-ui/core/styles';
-
-const defaultSearchStyles = theme => ({
-  main: {
-    display: 'flex',
-    flex: '1 0 auto',
-  },
-  searchIcon: {
-    color: theme.palette.text.secondary,
-    marginTop: '10px',
-    marginRight: '8px',
-  },
-  searchText: {
-    flex: '0.8 0',
-  },
-  clearIcon: {
-    '&:hover': {
-      color: theme.palette.error.main,
-    },
-  },
-});
 
 class App extends Component {
+  constructor(props){
+    super(props)
 
-  state = {
-    allCourses: [
-      {
-        courseId: "cpr", courseTitle: "First Aid and CPR", courseType: "Healthcare"
-      },
-      {
-        courseId: "derivatives", courseTitle: "Basics of Derivatives", courseType: "Finance"
-      },
-      {
-        courseId: "speak-english", courseTitle: "Speak English", courseType: "Soft Skills"
-      },
-      {
-        courseId: "test", courseTitle: "Derivatives Test", courseType: "Finance"
-      },
-    ],
-    filtered: ["First Aid and CPR", "Basics of Derivatives", "Speak English", "Derivatives Test"],
-    currentCourseType: "",
+    this.state = {
+      allCourses: [
+        {
+          courseId: "cpr", courseTitle: "First Aid and CPR", courseType: "Healthcare",
+          price: 100
+        },
+        {
+          courseId: "derivatives", courseTitle: "Basics of Derivatives", courseType: "Finance",
+          price: 200
+        },
+        {
+          courseId: "speak-english", courseTitle: "Speak English", courseType: "Soft Skills",
+          price: 300
+        },
+        {
+          courseId: "test", courseTitle: "Derivatives Test", courseType: "Finance", price: 400
+        },
+        {
+          courseId: "test2", courseTitle: "English 2", courseType: "Soft Skills", price: 200
+        },
+      ],
+      filteredObjects: [
+        {
+          courseId: "cpr", courseTitle: "First Aid and CPR", courseType: "Healthcare",
+          price: 100
+        },
+        {
+          courseId: "derivatives", courseTitle: "Basics of Derivatives", courseType: "Finance",
+          price: 200
+        },
+        {
+          courseId: "speak-english", courseTitle: "Speak English", courseType: "Soft Skills",
+          price: 300
+        },
+        {
+          courseId: "test", courseTitle: "Derivatives Test", courseType: "Finance", price: 400
+        },
+        {
+          courseId: "test2", courseTitle: "English 2", courseType: "Soft Skills", price: 200
+        },
+      ],
+      original: ["First Aid and CPR", "Basics of Derivatives", "Speak English", "Derivatives Test"],
+      filtered: ["First Aid and CPR", "Basics of Derivatives", "Speak English", "Derivatives Test"],
+      currentCourseType: "",
+      anchorEl: null,
+      anchorEl1: null,
+    }
   }
 
   filterByCourseType = (name, data) => {
     let result = data.filter(x => x.courseType.includes(name)) || []
-    if (result.length) {
-      result = Array.from(result.reduce((r, {courseTitle}) => r.add(courseTitle), new Set()))
-    }
     return result
+  };
+
+  filterByPriceLessThan = (name, data) => {
+    let result = data.filter( x => x.price < (name)) || []
+    return result
+  };
+
+
+  filterByPriceGreaterThan = (name, data) => {
+    let result = data.filter(x => x.price > (name)) || []
+    return result
+  };
+
+  filterLowPrices = () => {
+    this.setState({
+      filteredObjects: this.filterByPriceLessThan(250, this.state.filteredObjects)
+    })
+  };
+
+  filterHighPrices = () => {
+    this.setState({
+      filteredObjects: this.filterByPriceGreaterThan(250, this.state.filteredObjects)
+    })
   };
 
   filterCoursesFinance = () => {
     this.setState({
-      filtered: this.filterByCourseType("Finance", this.state.allCourses)
+      filteredObjects: this.filterByCourseType("Finance", this.state.filteredObjects)
     })
   };
 
   filterCoursesSS = () => {
     this.setState({
-      filtered: this.filterByCourseType("Soft Skills", this.state.allCourses)
+      filteredObjects: this.filterByCourseType("Soft Skills", this.state.filteredObjects)
     })
   };
 
   filterCoursesHealth = () => {
     this.setState ({
-      filtered: this.filterByCourseType("Healthcare", this.state.allCourses)
+      filteredObjects: this.filterByCourseType("Healthcare", this.state.filteredObjects)
     })
   };
 
 
-  render(){
-    const { classes, options, onHide, searchText } = this.props;
+  handleClick = (event) =>  {
+    this.setState({
+      anchorEl: event.currentTarget
+    })
+  };
 
-    const {allCourses, filtered} = this.state
+  handleClickPrice = (event) =>  {
+    this.setState({
+      anchorEl1: event.currentTarget
+    })
+  };
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null
+    })
+  };
+
+  handleClosePrice = () => {
+    this.setState({
+      anchorEl1: null
+    })
+  };
+
+  resetToOriginal = () => {
+    this.setState({
+      filteredObjects: this.state.allCourses
+    })
+  };
+
+  render(){
+
+
+    const {allCourses, filtered, filteredObjects, original, anchorEl, anchorEl1} = this.state
 
     const listItems = this.state.filtered.map(course => {
       return (
@@ -90,31 +153,34 @@ class App extends Component {
       )
     });
 
-    const courses = [
+    const getCourseTitles = this.state.filteredObjects.map(title => {
+      return (
+        <li key = {title.courseTitle}> {title.courseTitle} </li>
+      )
+    });
+
+    const menuItems = [
       {
-        courseId: "cpr", courseTitle: "First Aid and CPR", courseType: "Healthcare"
+        key: "1",
+        caption: "Finance",
+        onClick: () => this.filterCoursesFinance
       },
       {
-        courseId: "derivatives", courseTitle: "Basics of Derivatives", courseType: "Finance"
+        key:"2",
+        caption: "Soft Skills",
+        onClick: () => this.filterCoursesSS
       },
       {
-        courseId: "speak-english", courseTitle: "Speak English", courseType: "Soft Skills"
-      },
-      {
-        courseId: "test", courseTitle: "Derivatives Test", courseType: "Finance"
-      },
+        key: "3",
+        caption: "Healthcare",
+        onClick: () => this.filterCoursesHealth
+      }
     ];
 
-    const courseValues = [];
-    for (let i = 0; i < courses.length; i++){
-      courseValues[i] = Object.values(courses[i]);
-    }
+    console.log(getCourseTitles);
 
-    const final = [];
-    for (let j = 0; j < courseValues.length; j++){
-      final[j] = courseValues[j].slice(1);
-    }
-
+    console.log("Testing multiple filters")
+    console.log(this.state.filteredObjects);
 
     console.log(this.filterByCourseType("Finance", this.state.allCourses));
     console.log(this.filterByCourseType("Healthcare", this.state.allCourses));
@@ -124,16 +190,111 @@ class App extends Component {
     return (
     <div className="App">
 
+    <div>
+    <Button
+      aria-controls = "customized-menu"
+      aria-haspopup = "true"
+      variant = "contained"
+      color = "primary"
+      onClick = {this.handleClick}
+      >
+      Filter By Course Type
+    </Button>
+
+
+  <Menu id = "customized-menu"
+          anchorEl = {this.state.anchorEl}
+          keepMounted
+          open = {Boolean(anchorEl)}
+          onClose = {this.handleClose}>
+
+        <MenuItem button onClick = {this.filterCoursesFinance}>
+            <ListItemText primary = "Finance" />
+        </MenuItem>
+        <MenuItem button onClick = {this.filterCoursesSS}>
+            <ListItemText primary = "Soft Skills" />
+        </MenuItem>
+        <MenuItem button onClick = {this.filterCoursesHealth}>
+            <ListItemText primary = "Healthcare" />
+        </MenuItem>
+    </Menu>
+
+    <Button  color = "primary"
+              variant = "contained"
+              onClick = {this.handleClickPrice}
+    >
+          Filter By Price
+    </Button>
+
+    <Menu anchorEl = {this.state.anchorEl1}
+          keepMounted
+          open = {Boolean(anchorEl1)}
+          onClose = {this.handleClosePrice}>
+        <MenuItem button onClick = {this.filterLowPrices}>
+            <ListItemText primary = "Prices Lower Than 250" />
+        </MenuItem>
+        <MenuItem button onClick = {this.filterHighPrices}>
+            <ListItemText primary = "Price Higher Than 250" />
+        </MenuItem>
+    </Menu>
+
+    <Button
+        color = "secondary"
+        variant = "contained"
+        onClick = {this.resetToOriginal}>
+    Reset Filters
+    </Button>
+
+  
+    </div>
+ {/*
       <DropdownMenu triggerType = "text" trigger = "Filter by Course Type">
         <MenuItem text = "Finance" onClick = {this.filterCoursesFinance} />
         <MenuItem text = "Healthcare" onClick = {this.filterCoursesHealth} />
         <MenuItem text = "Soft Skills" onClick = {this.filterCoursesSS} />
       </DropdownMenu>
-  
+
+      <DropdownMenu triggerType = "text" trigger = "Filter by Price">
+        <MenuItem text = "Prices Less Than 250" onClick = {this.filterLowPrices} />
+        <MenuItem text = "Prices Higher Than 250" onClick = {this.filterHighPrices} />
+      </DropdownMenu>
+
+      <Chip variant = "outlined"
+            color = "primary"
+            onClick = {this.resetToOriginal}
+            label = "Reset"/>
+*/}
+
       <div>
 
-        {listItems}
+      <div>
+      {/*<Chip variant = "outlined"
+            label = "Finance"
+            size = "medium"
+            onClick = {this.filterCoursesFinance}/>
+      <Chip variant = "outlined"
+            color = "primary"
+            onClick = {this.filterCoursesHealth}
+            label = "Healthcare" />
+      <Chip variant = "outlined"
+            color = "secondary"
+            onClick = {this.filterCoursesSS}
+            label = "Soft Skills" />
+      <Chip variant = "outlined"
+            color = "primary"
+            onClick = {this.resetToOriginal}
+            label = "Reset"/>
 
+      <Chip onClick = {this.filterLowPrices}
+            label = "Price less than 250" />
+
+      <Chip onClick = {this.filterHighPrices}
+            label = "Price greater than 250" />
+            */}
+      </div>
+      <div>
+        {getCourseTitles}
+      </div>
       </div>
 
     </div>
@@ -141,4 +302,4 @@ class App extends Component {
 }
 }
 
-export default withStyles(defaultSearchStyles)(App);
+export default App;
